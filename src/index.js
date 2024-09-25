d34cd3e62c6b10dda2dc30ee12fa314ef9ddfa79
Eineleine <dsa210487@mail.ru>
2024-09-25 23:48:08 +0300

    Package.json is change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import 'character';
-import 'Bowerman';
-import 'Swordsman';
-import 'Magician';
-import 'Daemon';
-import 'Undead';
-import 'Zombie';
+import './character';
+import './Bowerman';
+import './Swordsman';
+import './Magician';
+import './Daemon';
+import './Undead';
+import './Zombie';
